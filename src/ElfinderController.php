c4a578e243d38cc91ae7eed746cf7f3f2986ec2b
Hilman Anshori <HilmanAnshori@users.noreply.github.com>
2016-08-25 04:26:21 +0700

    Update ElfinderController.php
    Edit to match my app

--- src/ElfinderController.php
+++ src/ElfinderController.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\Request;
 use League\Flysystem\Cached\CachedAdapter;
 use League\Flysystem\Cached\Storage\Memory;
 use League\Flysystem\Filesystem;
+use File;
 
 class ElfinderController extends Controller
 {
@@ -28,46 +29,46 @@ class ElfinderController extends Controller
     public function showIndex()
     {
         return $this->app['view']
-            ->make($this->package . '::elfinder')
-            ->with($this->getViewVars());
+        ->make($this->package . '::elfinder')
+        ->with($this->getViewVars());
     }
 
     public function showTinyMCE()
     {
         return $this->app['view']
-            ->make($this->package . '::tinymce')
-            ->with($this->getViewVars());
+        ->make($this->package . '::tinymce')
+        ->with($this->getViewVars());
     }
 
     public function showTinyMCE4()
     {
         return $this->app['view']
-            ->make($this->package . '::tinymce4')
-            ->with($this->getViewVars());
+        ->make($this->package . '::tinymce4')
+        ->with($this->getViewVars());
     }
 
     public function showCKeditor4()
     {
         return $this->app['view']
-            ->make($this->package . '::ckeditor4')
-            ->with($this->getViewVars());
+        ->make($this->package . '::ckeditor4')
+        ->with($this->getViewVars());
     }
 
     public function showPopup($input_id)
     {
         return $this->app['view']
-            ->make($this->package . '::standalonepopup')
-            ->with($this->getViewVars())
-            ->with(compact('input_id'));
+        ->make($this->package . '::standalonepopup')
+        ->with($this->getViewVars())
+        ->with(compact('input_id'));
     }
 
     public function showFilePicker($input_id)
     {
         $type = Request::input('type');
         return $this->app['view']
-            ->make($this->package . '::filepicker')
-            ->with($this->getViewVars())
-            ->with(compact('input_id','type'));
+        ->make($this->package . '::filepicker')
+        ->with($this->getViewVars())
+        ->with(compact('input_id','type'));
     }
 
     public function showConnector()
@@ -76,61 +77,88 @@ class ElfinderController extends Controller
         if (empty($roots)) {
             $dirs = (array) $this->app['config']->get('elfinder.dir', []);
             foreach ($dirs as $dir) {
+                $path = public_path($dir).'/'.\Auth::user()->id;
+                $url = url($dir).'/'.\Auth::user()->id;
+                if(!File::exists($path ))
+                    File::makeDirectory($path);
                 $roots[] = [
                     'driver' => 'LocalFileSystem', // driver for accessing file system (REQUIRED)
-                    'path' => public_path($dir), // path to files (REQUIRED)
-                    'URL' => url($dir), // URL to files (REQUIRED)
-                    'accessControl' => $this->app->config->get('elfinder.access') // filter callback (OPTIONAL)
-                ];
-            }
+                    'path' => $path, // path to files (REQUIRED)
+                    'URL' => $url, // URL to files (REQUIRED)
+                    'accessControl' => $this->app->config->get('elfinder.access'), // filter callback (OPTIONAL)  
+                    'tmbPath' => $path . '/_thumbs',  
+                    'tmbURL' => $url . '/_thumbs',
+                    'tmbSize' => '300px', 
+                    'tmbBgColor' => 'transparent',
+                    'attributes' => array(
+                        array(
+                            'pattern' => '~/_thumbs$~',
+                            'read' => false,
+                            'write' => false,
+                            'hidden' => true,
+                            'locked' => true
+                            )
 
-            $disks = (array) $this->app['config']->get('elfinder.disks', []);
-            foreach ($disks as $key => $root) {
-                if (is_string($root)) {
-                    $key = $root;
-                    $root = [];
-                }
-                $disk = app('filesystem')->disk($key);
-                if ($disk instanceof FilesystemAdapter) {
-                    $defaults = [
-                        'driver' => 'Flysystem',
-                        'filesystem' => $disk->getDriver(),
-                        'alias' => $key,
+                        )              
                     ];
-                    $roots[] = array_merge($defaults, $root);
+                }
+
+                $disks = (array) $this->app['config']->get('elfinder.disks', []);
+                foreach ($disks as $key => $root) {
+                    if (is_string($root)) {
+                        $key = $root;
+                        $root = [];
+                    }
+                    $disk = app('filesystem')->disk($key);
+                    if ($disk instanceof FilesystemAdapter) {
+                        $filesystem = $disk->getDriver();
+                        if (method_exists($filesystem, 'getAdapter')) {
+                            $adapter = $filesystem->getAdapter();
+                            if ( ! $adapter instanceof CachedAdapter) {
+                                $adapter = new CachedAdapter($adapter, new Memory());
+                                $filesystem = new Filesystem($adapter);
+                            }
+                        }
+                        $defaults = [
+                        'driver' => 'Flysystem',
+                        'filesystem' => $filesystem,
+                        'alias' => $key,
+                        ];
+                        $roots[] = array_merge($defaults, $root);
+                    }
                 }
             }
-        }
 
-        if (app()->bound('session.store')) {
-            $sessionStore = app('session.store');
-            $session = new LaravelSession($sessionStore);
-        } else {
-            $session = null;
-        }
+            if (app()->bound('session.store')) {
+                $sessionStore = app('session.store');
+                $session = new LaravelSession($sessionStore);
+            } else {
+                $session = null;
+            }
 
-        $rootOptions = $this->app->config->get('elfinder.root_options', array());
-        foreach ($roots as $key => $root) {
-            $roots[$key] = array_merge($rootOptions, $root);
-        }
+            $rootOptions = $this->app->config->get('elfinder.root_options', array());
+            foreach ($roots as $key => $root) {
+                $roots[$key] = array_merge($rootOptions, $root);
+            }
 
-        $opts = $this->app->config->get('elfinder.options', array());
-        $opts = array_merge($opts, ['roots' => $roots, 'session' => $session]);
+            $opts = $this->app->config->get('elfinder.options', array());
+            $opts = array_merge($opts, ['roots' => $roots, 'session' => $session]);
+            // dump($opts);exit();
 
         // run elFinder
-        $connector = new Connector(new \elFinder($opts));
-        $connector->run();
-        return $connector->getResponse();
-    }
-
-    protected function getViewVars()
-    {
-        $dir = 'packages/barryvdh/' . $this->package;
-        $locale = str_replace("-",  "_", $this->app->config->get('app.locale'));
-        if (!file_exists($this->app['path.public'] . "/$dir/js/i18n/elfinder.$locale.js")) {
-            $locale = false;
+            $connector = new Connector(new \elFinder($opts));
+            $connector->run();
+            return $connector->getResponse();
         }
-        $csrf = true;
-        return compact('dir', 'locale', 'csrf');
+
+        protected function getViewVars()
+        {
+            $dir = 'packages/barryvdh/' . $this->package;
+            $locale = str_replace("-",  "_", $this->app->config->get('app.locale'));
+            if (!file_exists($this->app['path.public'] . "/$dir/js/i18n/elfinder.$locale.js")) {
+                $locale = false;
+            }
+            $csrf = true;
+            return compact('dir', 'locale', 'csrf');
+        }
     }
-}
